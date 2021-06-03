8fcd277cdb918b11b8eee87afe5ae6c918f942f4
Artyom <artem.petrunin@list.ru>
2021-06-03 23:40:09 +0300

    final

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,30 @@
 import '../scss/app.scss';
 
-/* Your JS Code goes here */
+const mobileMenu = document.getElementById('mobileMenu');
+const hamburger = document.getElementById('hamburger');
+const mobileMenuInner = document.getElementById('mobileMenu__inner');
 
-/* Demo JS */
-import './demo.js';
+let isMenuOpened = false;
+
+function handleMenuClick() {
+  isMenuOpened = !isMenuOpened;
+
+  if (isMenuOpened) {
+    mobileMenu.classList.add('mobileMenuActive');
+  } else {
+    mobileMenu.classList.remove('mobileMenuActive');
+  }
+}
+
+hamburger.addEventListener('click', handleMenuClick);
+mobileMenuInner.addEventListener('click', handleMenuClick);
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
